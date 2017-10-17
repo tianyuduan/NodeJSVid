2cb07d77425e6fdaad9f611edb422193025043ef
Tianyu Duan <tianyuduan1991@gmail.com>
2017-10-17 12:54:23 -0700

    added flash session expression

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
-
+const flash = require('connect-flash');
+const session = require('express-session');
 const methodOverride = require('method-override');
 const app = express();
 
@@ -19,6 +20,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //middleware methodOverride
 app.use(methodOverride('_method'));
+//middleware session express
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUnintialized: true,
+}));
+
+app.use(flash());
+
+//global variables
+app.use(function(req, res, next){
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 //load Idea model
 require('./models/idea');
@@ -79,6 +96,7 @@ app.put('/ideas/:id', (req, res) => {
 
     idea.save()
     .then(idea => {
+      req.flash('success_msg', "Video idea updated");
       res.redirect('/ideas');
     }
   );
@@ -101,6 +119,7 @@ app.get('/ideas', (req, res)=>{
 app.delete('/ideas/:id', (req, res) => {
   Idea.remove({_id: req.params.id})
   .then(()=>{
+    req.flash('success_msg', "Video idea removed");
     res.redirect('/ideas');
   });
 });
@@ -129,6 +148,7 @@ app.post('/ideas', (req, res)=> {
   };
   new Idea(newUser).save()
   .then(idea => {
+    req.flash('success_msg', "Video idea added");
     res.redirect('/ideas');
   });
 }
